ef257cfda7f14df3c27043749655ce245626fe85
Wolf <macio.97.pavanello@gmail.com>
2020-04-11 01:11:43 +0200

    take samples along distance camera - top atmosphere

--- functions.py
+++ functions.py
@@ -19,7 +19,7 @@ def density_ratio(height):
 def phase_rayleigh(angle):
     # Rayleigh phase function
     return 3/(16*math.pi)*(1+(math.cos(angle))**2)
-
+'''
 def rayleigh_outscattering(lam, I, height, length):
     # optical depth
     for i in range(steps):
@@ -27,3 +27,19 @@ def rayleigh_outscattering(lam, I, height, length):
     # transmittance
     Tr = np.exp(-B*optical_depth)
     return I*Tr
+'''
+def sphere_intersection(pos, rot, center, radius):
+    a = rot[0]**2+rot[1]**2+rot[2]**2
+    b = -2*(rot[0]*(center[0]-pos[0])+rot[1]*(center[1]-pos[1])+rot[2]*(center[2]-pos[2]))
+    c = (center[0]-pos[0])**2+(center[1]-pos[1])**2+(center[2]-pos[2])**2-radius**2
+    t = (-b+math.sqrt(b**2-4*a*c))/2*a
+    point = np.array([pos[0]+rot[0]*t, pos[1]+rot[1]*t, pos[2]+rot[2]*t])
+    return point
+
+def distance_points(P1, P2):
+    distance = math.sqrt((P2[0]-P1[0])**2+(P2[1]-P1[1])**2+(P2[2]-P1[2])**2)
+    return distance
+
+def normalize(lat, lon):
+    nor = np.array([math.cos(lat)*math.cos(lon), math.cos(lat)*math.sin(lon), math.sin(lat)])
+    return nor
--- main.py
+++ main.py
@@ -8,33 +8,64 @@ import variables as var
 
 
 # Geometry
-height = 1                          # position of camera from sea level (m)
-cam_pos = var.Re+height             # position of the camera
-cam_rot = (0, 0)                    # rotation of the camera (from 0 to 89,9 degrees, from -180 to 180 degrees)
+height = 0                          # position of camera from sea level (m) (max: 60km)
+cam_pos = np.array([0, 0, var.Re+height], dtype=np.int64)
+earth_center = np.array([0, 0, 0])
+samples = 2
+cam_lat = math.radians(0)
+cam_lon = math.radians(0)
+sun_lat = math.radians(40)
+sun_lon = math.radians(0)
 
-latitude = 89.999
 
-# calculate distance camera - top atmosphere AB
-if latitude == 0:
-    AB = math.sqrt((var.Ra)**2-(cam_pos)**2)
-if latitude > 0:
-    # angle OAB
-    alpha = math.radians(90+latitude)
-    # angle OBA
-    beta = math.asin(cam_pos*math.sin(alpha)/var.Ra)
-    # angle AOB
-    gamma = math.pi-alpha-beta
-    AB = cam_pos*math.sin(gamma)/math.sin(beta)
+# normalize camera rotation
+cam_rot = fun.normalize(cam_lat, cam_lon)
+# normalize sun rotation
+sun_rot = fun.normalize(sun_lat, sun_lon)
+# intersection between camera and top of atmosphere
+P = fun.sphere_intersection(cam_pos, cam_rot, earth_center, var.Ra)
+# distance from camera to top of atmosphere
+AB = fun.distance_points(cam_pos, P)
+# length of each step
+AP = AB/samples
 
-print(AB)
 
+for i in range(samples):
+    # distance between each sample
+    d = i*AP+AP/2
+    # point for inscattering
+    P1 = cam_pos+d*cam_rot
+
+'''
+APo = AB/steps
+PoH = math.sin(latitude)*APo
+AH = math.cos(latitude)*APo
+# angle AHO
+ang1 = math.atan(cam_pos/AH)
+# angle HOA
+ang2 = math.pi-math.pi/2-ang1
+# angle OPoCo
+alpha = ang2+math.pi/2+sun_rot[0]
+print("alpha ", alpha)
+OPo = math.sqrt(PoH**2+AH**2)
+print("OPo ", OPo)
+# angle OCoPo
+beta = math.asin(cam_pos*math.sin(alpha)/OPo)
+# angle PoOCo
+gamma = math.pi-alpha-beta
+# distance from point P to top of atmosphere C
+PC = OPo*math.sin(gamma)/math.sin(beta)
+print(PC)
+'''
+
+'''
 # Image
 pixelsx = 256
 pixelsy = 64
 
 img = Image.new('RGB', (pixelsx, pixelsy), "black")
 pixels = img.load()
-
+'''
 #for i in range(img.size[0]):
 #    for j in range(img.size[1]):
         # calculate Rayleigh scattering
