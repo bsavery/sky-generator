4d951ad376f7b4681f397329c6c4269a8fa5c267
Wolf <macio.97.pavanello@gmail.com>
2020-04-14 13:15:28 +0200

    allow use of multiple cores

--- main.py
+++ main.py
@@ -5,33 +5,32 @@ from PIL import Image
 import spectrum as spec
 import functions as fun
 import variables as var
-import threading
-from multiprocessing import Pool
+from multiprocessing import Process, Array
 
 
 # Geometry
 height = 0                                                  # position of camera from sea level (m) (max: 60km)
 cam_pos = np.array([0, 0, var.Re+height], dtype=np.int64)   # position of camera
 earth_center = np.array([0, 0, 0])                          # center of Earth
-samples = 30                                                # number of divisions for the rays
 # Sun rotation
 sun_lat = math.radians(40)
 sun_lon = math.radians(0)
 # normalize sun rotation
 sun_rot = fun.normalize(sun_lat, sun_lon)
-# number of threads (the real number will be nthreads**2)
-nthreads = 4
-
-
+# divisions of the rays
+samples = 40
+# number of processes (final number is squared)
+nproc = 6
 # image size
-pixelsx = 128
-pixelsy = 64
+pixelsx = 256
+pixelsy = 128
 
+# image definition
 img = Image.new('RGB', (pixelsx, pixelsy), "black")
 pixels = img.load()
 
 
-def calc_pixel(xmin, xmax, ymin, ymax):
+def calc_pixel(xmin, xmax, ymin, ymax, pix):
     for i in range(xmin, xmax):
         for j in range(ymin, ymax):
             # camera rotation
@@ -41,19 +40,29 @@ def calc_pixel(xmin, xmax, ymin, ymax):
             cam_rot = fun.normalize(cam_lat, cam_lon)
             rgb = fun.get_rgb(sun_rot, cam_pos, cam_rot, earth_center, samples)
             # print to the final pixels
-            pixels[i, j] = rgb
+            pix[i*3*pixelsy+j*3] = rgb[0]
+            pix[i*3*pixelsy+j*3+1] = rgb[1]
+            pix[i*3*pixelsy+j*3+2] = rgb[2]
 
 
 def multithread():
-    threads = []
-    for i in range(nthreads):
-        for j in range(nthreads):
-            t = threading.Thread(target=calc_pixel, args=(
-                int((pixelsx/nthreads)*j), int((pixelsx/nthreads)*(j+1)), int((pixelsy/nthreads)*i), int((pixelsy/nthreads)*(i+1))
+    process = []
+    pix = Array('i', pixelsx*pixelsy*3)
+    for i in range(nproc):
+        for j in range(nproc):
+            p = Process(target=calc_pixel, args=(
+                int((pixelsx/nproc)*j), int((pixelsx/nproc)*(j+1)), int((pixelsy/nproc)*i), int((pixelsy/nproc)*(i+1)), pix,
             ))
-            threads.append(t)
-            t.start()
-            t.join()
+            process.append(p)
+            p.start()
+    
+    for p in process:
+        p.join()
+    #print(pix[:])
+    for i in range(pixelsx):
+        for j in range(pixelsy):
+            pixels[i, j] = (pix[i*3*pixelsy+j*3], pix[i*3*pixelsy+j*3+1], pix[i*3*pixelsy+j*3+2])
+    
     # open image
     img.show()
 
