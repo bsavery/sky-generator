d8165b616f30fc994831030e53f198daa4bfb5cd
Wolf <macio.97.pavanello@gmail.com>
2020-04-19 17:29:07 +0200

    optimization: render half sky and mirror it along the sun

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ height = 1                                                  # position of camera
 cam_pos = np.array([0, 0, var.Re+height], dtype=np.int64)   # position of camera
 earth_center = np.array([0, 0, 0])                          # center of Earth
 # Sun rotation
-sun_lat = math.radians(5)
+sun_lat = math.radians(60)
 sun_lon = math.radians(0)
 # normalize sun rotation
-sun_rot = fun.normalize(sun_lat, sun_lon)
+sun_rot = fun.normalize(sun_lat, 0)
 # divisions of the rays (more divisions make more accurate results)
 samples = 20
 # number of processes (squared number must be near the number of logic processors of the CPU)
@@ -27,6 +27,8 @@ pixelsy = 64
 # image definition
 img = Image.new('RGB', (pixelsx, pixelsy), "black")
 pixels = img.load()
+img_shifted = Image.new('RGB', (pixelsx, pixelsy), "black")
+pixels_shifted = img_shifted.load()
 
 
 def calc_pixel(xmin, xmax, ymin, ymax, pix):
@@ -46,13 +48,14 @@ def calc_pixel(xmin, xmax, ymin, ymax, pix):
 
 def multiprocess():
     processes = []
+    halfx = int(pixelsx/2)
     # create shared memory array (that can be accessed by multiple processes at the same time)
-    pix = Array('i', pixelsx*pixelsy*3)
+    pix = Array('i', halfx*pixelsy*3)
     # split the image in nproc**2 processes
     for i in range(nproc):
         for j in range(nproc):
             p = Process(target=calc_pixel, args=(
-                int((pixelsx/nproc)*j), int((pixelsx/nproc)*(j+1)), int((pixelsy/nproc)*i), int((pixelsy/nproc)*(i+1)), pix,
+                int((halfx/nproc)*j), int((halfx/nproc)*(j+1)), int((pixelsy/nproc)*i), int((pixelsy/nproc)*(i+1)), pix,
             ))
             processes.append(p)
             p.start()
@@ -60,11 +63,25 @@ def multiprocess():
     for p in processes:
         p.join()
     # print to final pixels
-    for i in range(pixelsx):
+    for i in range(halfx):
         for j in range(pixelsy):
             pixels[i, j] = (pix[i*3*pixelsy+j*3], pix[i*3*pixelsy+j*3+1], pix[i*3*pixelsy+j*3+2])
+            pixels[pixelsx-i-1, j] = (pix[i*3*pixelsy+j*3], pix[i*3*pixelsy+j*3+1], pix[i*3*pixelsy+j*3+2])
+    # shift pixels with sun lon change
+    shift = int(sun_lon/(math.pi*2)*pixelsx)
+    s = 0
+    for x in range(pixelsx):
+        for y in range(pixelsy):
+            if x+shift<pixelsx:
+                pixels_shifted[x+shift, y] = pixels[x, y]
+            else:
+                if s<shift:
+                    pixels_shifted[s, y] = pixels[x, y]
+                    if y==pixelsy-1:
+                        s += 1
+
     # open image
-    img.show()
+    img_shifted.show()
 
 
 # multiprocessing
